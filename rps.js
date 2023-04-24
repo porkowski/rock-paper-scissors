2a53ccd821f2044a3f4099194590fbd194db93a5
Porkowski <borkowskipatryk10@gmail.com>
2023-04-24 19:47:06 -0400

    Switch if/else statements in Round() to switch statements. Round() now properly states whether game was won or lost.

--- rps.js
+++ rps.js
@@ -6,13 +6,68 @@ let playerChoice = pChoice.toLowerCase();
 
 let computerChoice = function () {
 return cChoice = choices[Math.floor(Math.random()*choices.length)];
+
+}
+computerChoice();
+
+
+function Round () {
+
+    
+    console.log(playerChoice);
+    console.log(cChoice);
+
+switch(true) {
+
+    case (playerChoice === "rock" && cChoice === "scissors") :
+    console.log("You win! Rock beats scissors!");
+    break;    
+    
+    case (playerChoice === "rock"&& cChoice === "paper") :
+    console.log("You lose! Paper beats rock!");
+    break;
+
+    case (playerChoice === "paper" && cChoice === "scissors") :
+    console.log("You lose! Scissors beats paper!");
+    break;
+
+    case (playerChoice === "paper" && cChoice === "rock") :
+    console.log("You win! Paper beats rock!");
+    break;
+
+    case (playerChoice === "scissors" && cChoice === "rock") :
+    console.log("You lose! Rock beats scissors!");
+    break;
+
+    case (playerChoice === "scissors" && cChoice === "paper") :
+    console.log("You win! Scissors beats paper!");
+    break;
+
+    case (playerChoice === "rock" && cChoice === "paper") :
+    console.log("You lose! Paper beats rock!");
+    break;
+
+    case (playerChoice === cChoice) :
+    console.log("It's a tie! Rematch if you please!");
+    break;
+}
 }
 
-
-function Round (playerChoice, cChoice) {
-
+Round();
 
 
+
+
+
+
+
+
+
+
+
+
+// BELOW IS MY ATTEMPT AT IF/ ELSE STATEMENTS INSIDE ROUND(), WHICH DID NOT WORK.
+/*
 if (playerChoice === "rock", cChoice === "scissors") {
     console.log("You win! Rock beats scissors!");
     
@@ -33,10 +88,10 @@ if (playerChoice === "rock", cChoice === "scissors") {
 
 }  else if (playerChoice === "rock", cChoice === "paper") {
     console.log("You lose! Paper beats rock!");
+
 } else if (playerChoice === cChoice) {
     console.log("It's a tie! Rematch if you please!");
 }
 }
+*/
 
-computerChoice();
-Round(); 
